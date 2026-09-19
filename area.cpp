5d50e9a579f34669a387d5c9dcfa900c29e9123c
Praveen kumar <pal0163392@gmail.com>
2026-09-19 12:08:03 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int x , y;
+    cout<<"enter the number:";
+    cin >>x;
+    cout<<"enter the number:";
+    cin>>y;
+ cout<<x+y <<endl;
+ cout<<x*y<<endl;
+ cout<<x/y;
+
+
+
+
+}
